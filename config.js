c7468f18bf64bdf2f760c0b29a4b89de77fe8445
ROYALTECH24 <roye10969@gmail.com>
2025-04-01 13:34:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
 //  PUT YOUR SETTINGS BELOW 👇
 //===========================
     
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~WX4xACCL#nphCeeHMJzdrp-_C9WluXb8HouPgMXfrkm8nbHwoqkA",
 // add your Session Id 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true or false status auto seen
@@ -29,7 +29,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "18062212660",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254799211357",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Mr Frank",
 // add bot owner name
@@ -43,7 +43,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
@@ -53,7 +53,7 @@ AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
